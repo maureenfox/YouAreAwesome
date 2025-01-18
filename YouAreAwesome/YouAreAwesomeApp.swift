fc2febc088b23ed0da65b6247a8a3d2db626d7d2
Maureen Fox <maureenfox2022@gmail.com>
2025-01-18 14:38:28 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Maureen Fox on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
